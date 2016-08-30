f878a0c65542ff5af6820c7a5cc1fb1ebc07a629
Adrian Wright <rasswright@users.noreply.github.com>
2016-08-30 11:41:13 -0400

    Update MTFS-linux.sh

--- MTFS-linux.sh
+++ MTFS-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ping -c4 8.8.8.8
 function main {
 	
 	#set time/date
@@ -16,13 +16,13 @@ function main {
 	echo " "
 
 	#manual config edits
-    nano /etc/apt/sources.list	   #check for malicious sources
-    nano /etc/resolv.conf		   #make sure if safe, use 8.8.8.8 for name server
-    nano /etc/hosts  			   #make sure is not redirecting
-    nano /etc/rc.local 			   #should be empty except for 'exit 0'
-    nano /etc/sysctl.conf 		   #change net.ipv4.tcp_syncookies entry from 0 to 1
-    nano /etc/lightdm/lightdm.conf #allow_guest=false, remove autologin
-    nano /etc/ssh/sshd_config 	   #Look for PermitRootLogin and set to no
+#    nano /etc/apt/sources.list	   #check for malicious sources
+#    nano /etc/resolv.conf		   #make sure if safe, use 8.8.8.8 for name server
+#    nano /etc/hosts  			   #make sure is not redirecting
+#    nano /etc/rc.local 			   #should be empty except for 'exit 0'
+#    nano /etc/sysctl.conf 		   #change net.ipv4.tcp_syncookies entry from 0 to 1
+#    nano /etc/lightdm/lightdm.conf #allow_guest=false, remove autologin
+#   nano /etc/ssh/sshd_config 	   #Look for PermitRootLogin and set to no
 	
 	echo "Manual config edited"
 	
