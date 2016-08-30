5cad98fdff07480eef218b846861c08ca14ad14a
Adrian Wright <rasswright@users.noreply.github.com>
2016-08-30 09:00:45 -0400

    Update MTFS-linux.bash

--- MTFS-linux.bash
+++ MTFS-linux.bash
@@ -6,33 +6,39 @@ function main {
 	now="$(date +'%d/%m/%Y %r')"
 
 	#ensure start-up
+	echo " "
 	
-	
-	
+	cat MTFS.txt
 	echo "I'm awake, it's ($now), in my opinion it's too damn early for this"
-	echo "run as 'sudo sh MTFS-linux.sh 2>&1 | tee output.log' to output the console output to a log file."
+	echo "run as {sudo xterm -hold -e 'sh MTFS-linux.sh 2>&1 | tee output.log'} to output the console output to a log file."
 	echo "Big thanks to JoshuaTatum"
 
+	echo " "
+
 	#manual config edits
-    nano /etc/apt/sources.list #check for malicious sources
-    nano /etc/resolv.conf #make sure if safe, use 8.8.8.8 for name server
-    nano /etc/hosts #make sure is not redirecting
-    nano /etc/rc.local #should be empty except for 'exit 0'
-    nano /etc/sysctl.conf #change net.ipv4.tcp_syncookies entry from 0 to 1
+    nano /etc/apt/sources.list	   #check for malicious sources
+    nano /etc/resolv.conf		   #make sure if safe, use 8.8.8.8 for name server
+    nano /etc/hosts  			   #make sure is not redirecting
+    nano /etc/rc.local 			   #should be empty except for 'exit 0'
+    nano /etc/sysctl.conf 		   #change net.ipv4.tcp_syncookies entry from 0 to 1
     nano /etc/lightdm/lightdm.conf #allow_guest=false, remove autologin
-    nano /etc/ssh/sshd_config #Look for PermitRootLogin and set to no
+    nano /etc/ssh/sshd_config 	   #Look for PermitRootLogin and set to no
 	
 	echo "Manual config edited"
 	
 	#installs
-	apt-get -V -y install firefox hardinfo chkrootkit iptables portsentry lynis ufw gufw sysv-rc-conf nessus clamav
+	apt-get -V -y install firefox hardinfo chkrootkit iptables portsentry lynis ufw gufw sysv-rc-conf nessus clamav firestarter
 	apt-get -V -y install --reinstall coreutils
-	apt-get install firestarter
+	freshclam
 	apt-get update
 	apt-get upgrade
 	apt-get dist-upgrade
 	
-	
+	#antivirus
+	gnome-terminal -e 'clamscan -i -r --remove=yes / '
+
+	echo "That should de-gunk the processor"
+
 	#network stuff
 	iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 23 -j DROP         #Block Telnet
     iptables -A INPUT -p tcp -s 0/0 -d 0/0 --dport 2049 -j DROP       #Block NFS
@@ -49,7 +55,8 @@ function main {
 	ufw deny 2049	#Block NFS
 	ufw deny 515	#Block printer port
 	ufw deny 111	#Block Sun rpc/NFS
-	
+	lsof  -i -n -P
+	netstat -tulpn
 	
 	#media file deletion
     find / -name '*.mp3' -type f -delete
@@ -67,20 +74,25 @@ function main {
     find /home -name '*.jpg' -type f -delete
 	find /home -name '*.jpeg' -type f -delete
 	
-	echo "crap should be gone now"
+	echo "media crap should be gone now"
 	
 	#user upgrade
-	sudo passwd
 	passwd
 	passwd -l root
 	
+	#yo dog heard you like logs, so I put logs in a log so you can log while you log
+
+	gnome-terminal -e 'echo "Whatever we end up doing for a log solution"'
+
+
+	#show precesses
+	gnome-terminal -e 'ps | echo "look through this for anything out of the ordanary"'
+	gnome-terminal -e 'top | echo "look through this for anything out of the ordanary"'
+	
+	#closing
+	now="$(date +'%d/%m/%Y %r')"
+	echo "I give up, I ran out of stuff to do at ($now)"
+
 }
 
 
-
-___  ___    ______    _   _              _   _   ___  __   __
-|  \/  |    |  ___|  | | | |            | | | | / _ \ \ \ / /
-| .  . | ___| |_ __ _| |_| |_ ___ _ __  | |_| |/ /_\ \ \ V / 
-| |\/| |/ __|  _/ _` | __| __/ _ \ '__| |  _  ||  _  | /   \ 
-| |  | | (__| || (_| | |_| ||  __/ |    | | | || | | |/ /^\ \
-\_|  |_/\___\_| \__,_|\__|\__\___|_|    \_| |_/\_| |_/\/   \/
